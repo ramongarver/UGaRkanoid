bf00a370b5cd0e232d3586cec096debcf97b5127
daniharo <daniharopadul@gmail.com>
2021-06-10 14:44:03 +0200

    Diagrama y descripción añadidos

--- src/LevelGenerator.js
+++ src/LevelGenerator.js
@@ -31,8 +31,6 @@ class LevelGenerator {
     this.rowsNumber = rowsNumber;
     this.columnsNumber = columnsNumber;
     this.maximumHardness = maximumHardness;
-
-    BrickTypes.keys;
   }
 
   generateLevel() {
